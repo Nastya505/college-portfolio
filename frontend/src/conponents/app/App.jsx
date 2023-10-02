198b3b68c282a0360c8aef332e1ef03c99eb4ca9
Anastasia <555.555.555.anastasie@gmail.com>
2023-10-02 18:56:57 +0300

    create frontend

---
+++ frontend/src/conponents/app/App.jsx
@@ -0,0 +1,12 @@
+import './App.module.css';
+import Header from '../header/Header.jsx';
+
+function App() {
+  return (
+   <>
+      <Header/>
+   </>
+  );
+}
+
+export default App;
---
+++ frontend/src/conponents/header/Header.jsx
@@ -0,0 +1,11 @@
+import styles from "../header/Header.module.css"
+
+function Header () {
+    return(
+        <>
+            <h1 className={styles.title}>Работы студентов</h1>
+        </>
+    )
+}
+
+export default Header;
